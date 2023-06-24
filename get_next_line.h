7393974744541a939cedbbbb2ecb69b53a599d24
Uetor <pgonzlezcarvajal@gmail.com>
2023-06-24 05:03:04 +0200

    Get next line

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int	    ft_strlen(char *str);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,81 @@
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *) dest;
+	s = (unsigned char *) src;
+	if (dest == NULL && src == NULL)
+		return (dest);
+	while (n > 0)
+	{
+		*d = *s;
+		d++;
+		s++;
+		n--;
+	}
+	return (dest);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		i;
+
+	i = ft_strlen(s) +1;
+	str = (char *)malloc(sizeof(char) * i);
+	if (str == NULL)
+		return (0);
+	str = ft_memcpy(str, s, i);
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
